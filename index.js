55f6118b1c812c31398a5668d62b9b36d4e69413
Ayanda Mathiso <mathisoayanda@gmail.com>
2023-02-23 23:08:26 +0200

    YelpCamp basic CRUD

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose')
+const methodOverride = require('method-override')
+mongoose.set('strictQuery', true);
+const Campground = require('./models/campground');
+const { ADDRGETNETWORKPARAMS } = require('dns');
+
+mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log('Database connected')
+})
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
+app.get('/', (req, res) => {
+    res.render('Home');
+});
+app.get('/campgrounds', async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', { campgrounds });
+});
+
+app.get('/campground/new', (req, res) => {
+    res.render('campgrounds/new');
+});
+app.post('/campgrounds', async (req, res) => {
+    const campground = new Campground.findById(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+});
+
+app.get('/campgrounds/:id', async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/show', { campground });
+});
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/edit', { campground });
+});
+
+app.get('/campgrounds/:id', async (req, res) => {
+    const {id} = req.params;
+    await Campground.findByIdAndUpdate(id,{...req.body.campground});
+    res.redirect(`/campgrounds/${campground._id}`);
+})
+
+
+
+
+
+app.listen(8080, () => {
+    console.log('Serving port 8080');
+});
---
+++ models/campground.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+mongoose.set('strictQuery', true);
+
+const CampgroundSchema = new Schema({
+    title: String,
+    price: String,
+    description: String,
+    location: String
+});
+
+module.exports = mongoose.model('Campground', CampgroundSchema);
